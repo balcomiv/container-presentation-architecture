28a5c8ad043e5958907d79f14b22f94f2127d8d0
BalcomIV <35431931+balcomiv@users.noreply.github.com>
2021-01-06 05:35:32 -0500

    update - import path to fix stackblitz run

--- src/app/tenant/services/tenant-detail.service.ts
+++ src/app/tenant/services/tenant-detail.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject, Observable } from 'rxjs';
 import { switchMap, tap } from 'rxjs/operators';
-import { ApiService } from 'src/app/api/api.service';
+import { ApiService } from '../../api/api.service';
 import { Tenant } from '../interfaces/tenant.interface';
 
 @Injectable({
