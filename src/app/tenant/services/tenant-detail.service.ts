5fcb866247dd5d8523a639d8d64a339321207822
BalcomIV <35431931+balcomiv@users.noreply.github.com>
2021-02-10 13:57:25 -0500

    update - removing commented code

--- src/app/tenant/services/tenant-detail.service.ts
+++ src/app/tenant/services/tenant-detail.service.ts
@@ -42,29 +42,3 @@ export class TenantDetailService {
     }, 1000);
   }
 }
-
-// export class TenantDetailService {
-//   loading: Observable<boolean>;
-//   tenant: Observable<Tenant>;
-
-//   private _needNewTenantData = new BehaviorSubject<void>(undefined);
-//   private _loadingSubj = new BehaviorSubject<boolean>(true);
-
-//   constructor(private apiService: ApiService) {
-//     this.loading = this._loadingSubj.asObservable();
-
-//     this.tenant = this._needNewTenantData.pipe(
-//       switchMap(() => {
-//         return this.apiService.getTenant();
-//       }),
-//       tap(() => this._loadingSubj.next(false))
-//     );
-//   }
-
-//   save(tenant: Tenant): void {
-//     this._loadingSubj.next(true);
-//     this.apiService.saveTenant(tenant).subscribe(() => {
-//       this._needNewTenantData.next(undefined);
-//     });
-//   }
-// }
